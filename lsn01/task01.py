3409814c4359629a810a162d87cb440bba09c5e3
bratick@mail.ru <GITbrat2018!>
2021-11-24 21:58:20 +0300

    done 2 task

---
+++ lsn01/task01.py
@@ -0,0 +1,7 @@
+a = 1
+b = 2
+print(f"a={a}, b={b}, сумма {a + b}")
+
+x1 = int(input("Enter x1 >>>"))
+x2 = int(input("Enter x2 >>>"))
+print(f'Сложение:{x1 + x2}, вычитание {x1 - x2}, умножение {x1 * x2}, деление {x1 / x2}')
