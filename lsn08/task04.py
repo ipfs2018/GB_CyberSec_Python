9ede6650797769f1295c0fd7c6a271f23f36ec78
bratick@mail.ru <GITbrat2018!>
2021-12-23 20:05:42 +0300

    done task #1,2,3

--- lsn08/task04.py
+++ lsn08/task04.py
@@ -5,3 +5,57 @@
 В базовом классе определить параметры, общие для приведенных типов.
 В классах-наследниках реализовать параметры, уникальные для каждого типа оргтехники.
 '''
+
+
+class Warehouse:
+    capacity: int
+    items: int
+
+    def __init__(self, items):
+        self.items = items
+
+
+class OfficeEquipment:
+    name: str
+    brand: str
+    year: str
+
+    def __init__(self, name, brand, year):
+        self.name = name
+        self.brand = brand
+        self.year = year
+
+    def __repr__(self):
+        return f'{self.name}-{self.brand}-{self.year}'
+
+
+class Printer(OfficeEquipment):
+    def __init__(self, name, brand, year):
+        super().__init__(name, brand, year)
+
+    @property
+    def do():
+        return 'Печатает'
+
+
+class Scanner(OfficeEquipment):
+    def __init__(self,  name, brand, year):
+        super().__init__(name, brand, year)
+
+    @property
+    def do():
+        return 'Сканирует.'
+
+
+class Copier(OfficeEquipment):
+    def __init__(self,  name, brand, year):
+        super().__init__(name, brand, year)
+
+    @property
+    def do(self):
+        return ' копирует.'
+
+
+A = Copier('mf333', 'Canon', '1999')
+print(A)
+print(A.__name__)
