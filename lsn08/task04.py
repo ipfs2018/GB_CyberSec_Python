beff9ebc9db23c0c36c9a326df92dd69e019983d
bratick@mail.ru <GITbrat2018!>
2021-12-24 15:54:11 +0300

    done task #1,2,3,4

--- lsn08/task04.py
+++ lsn08/task04.py
@@ -6,6 +6,8 @@
 В классах-наследниках реализовать параметры, уникальные для каждого типа оргтехники.
 '''
 
+# move_to
+#  проверка от списания сверх хранения
 
 class Warehouse:
     capacity: int
@@ -13,7 +15,18 @@ class Warehouse:
     def __init__(self, capacity: int = 10):
         self._warehouse_dict = {}
         self.capacity = capacity
-        print(f'Создан склад на {self.capacity} мест.\n')
+        print(f'Создан склад на {self.capacity} мест.')
+
+    def info(self):
+        if len(self._warehouse_dict.items())==0:
+            print(f'Склад пуст. Свободны {self.capacity} мест.')
+        else:
+            print(f'На складе хранится:')
+            for item in self._warehouse_dict.items():
+                tmp1 = list(item)
+                tmp2 = tmp1[1]
+                tmp3 = tmp2[0]
+                print(f'{tmp3[1]} {tmp3[0]}, количество {tmp2[1]}.')
 
     def add_to(self, OffEquips: str, q: int):
         self.OffEquips = OffEquips
@@ -27,7 +40,8 @@ class Warehouse:
             print(
                 f'Оборудование {tmp[1]}:{tmp[0]} в количестве {q} успешно размещено на складе.\nНа складе свободно {self.capacity} мест.\n')
         elif self.capacity <= 0:
-            print(f'Невозможно разместить оборудование {tmp[1]}:{tmp[0]} в количестве {q} на складе: нет свободного места.\n')
+            print(
+                f'Невозможно разместить оборудование {tmp[1]}:{tmp[0]} в количестве {q} на складе: нет свободного места.\n')
             self.capacity = 0
         else:
             self._warehouse_dict.setdefault(self.OffEquips.name, [])
@@ -41,13 +55,11 @@ class Warehouse:
         self.department = department
         self.q = q
         if self._warehouse_dict[name_eq]:
-            # self._warehouse_dict.setdefault(name_eq).pop(0)
-            tmp=self._warehouse_dict.get(name_eq)
-            tmp[1]-=q
-            # print(f'move_to print>>>{tmp}')
-
+            tmp = self._warehouse_dict.get(name_eq)
+            tmp[1] -= q
         self.capacity += q
-        print(f'Оборудование {self.name_eq} в количестве {self.q} передано в {self.department}.\n На складе {self.capacity} свободных мест.')
+        print(
+            f'Оборудование {self.name_eq} в количестве {self.q} передано в {self.department}.\n На складе {self.capacity} свободных мест.\n')
 
 
 class OfficeEquipment:
@@ -93,25 +105,30 @@ class Copier(OfficeEquipment):
     def do(self):
         return 'Делает копии документов.'
 
+''' Блок определения и печати характеристик склада '''
 W = Warehouse(10)
+W.info()
+print('>>>')
+
+''' Блок определения и печати характеристик устройств '''
 P = Printer('color1234', 'HP', '2000', True)
 S = Scanner('z077', 'Zebra', '2020', '800x600')
 C = Copier('mf333', 'Canon', '1999', 15)
-
 print(f'Устройство {C}.\n{C.do}\nСкорость копирования: {C.speed} лист/минута.\n')
 print(f'Устройство {P}.\n{P.do}\nДвустронняя печать: {P.duplex}.\n')
 print(f'Устройство {S}.\n{S.do}\nРазрешение: {S.resolution} точек/дюйм.\n')
+print('>>>')
 
-
+''' Блок размещения устройств на складе '''
 W.add_to(P, 4)
 W.add_to(S, 3)
 W.add_to(C, 6)
+W.info()
 # print(f'TP >>> {W._warehouse_dict} осталось свободных мест на складе:{W.capacity}')
+print('>>>')
 
-W.move_to('color1234',2,'АСУ')
+
+W.move_to('color1234', 2, 'УАСУ')
+W.move_to('z077', 11, 'Бухгалтерия')
 # print(f'TP >>> {W._warehouse_dict} осталось свободных мест на складе:{W.capacity}')
-for item in W._warehouse_dict.items():
-    tmp=list(item)
-    # print(tmp[0])
-    for each in tmp:
-        print(each)
+W.info()
